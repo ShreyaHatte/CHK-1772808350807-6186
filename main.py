5dedde839e8e736fa7bb2d0c87425119404568c9
ShreyaHatte <shreyahatte04@gmail.com>
2026-03-11 00:48:02 +0530

    solved merge conf

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 import tkinter as tk
 from ui_action import encode_action, decode_action
-from database import create_db
-from database import get_messages
+from database import create_db, get_messages
 from tkinter import ttk
 
 create_db()
 
 
 def show_history():
-
     history_window = tk.Toplevel(window)
     history_window.title("Message History")
     history_window.geometry("700x400")
@@ -28,7 +25,8 @@ def show_history():
         tree.insert("", tk.END, values=row)
 
     tree.pack(fill="both", expand=True)
-    
+
+
 window = tk.Tk()
 window.title("Secret Message Translation Station")
 window.geometry("500x350")
@@ -122,62 +120,6 @@ history_button = tk.Button(
     fg="white",
     command=show_history
 )
-
 history_button.pack(pady=5)
 
-
-
 window.mainloop()
-
-=======
-import tkinter as tk
-from ui_action import encode_action, decode_action
-
-
-window = tk.Tk()
-window.title("Secret Message Translation Station")
-window.geometry("500x350")
-
-
-title = tk.Label(window, text="Secret Message Translator", font=("Arial", 16))
-title.pack(pady=10)
-
-
-message_label = tk.Label(window, text="Enter Message")
-message_label.pack()
-
-message_entry = tk.Entry(window, width=40)
-message_entry.pack(pady=5)
-
-
-key_label = tk.Label(window, text="Enter Secret Key")
-key_label.pack()
-
-key_entry = tk.Entry(window, width=20)
-key_entry.pack(pady=5)
-
-
-encode_button = tk.Button(
-    window,
-    text="Encode",
-    bg="lightblue",
-    command=lambda: encode_action(message_entry, key_entry, result_label)
-)
-encode_button.pack(pady=5)
-
-
-decode_button = tk.Button(
-    window,
-    text="Decode",
-    bg="lightgreen",
-    command=lambda: decode_action(message_entry, key_entry, result_label)
-)
-decode_button.pack(pady=5)
-
-
-result_label = tk.Label(window, text="", font=("Arial", 12))
-result_label.pack(pady=10)
-
-
-window.mainloop()
->>>>>>> e21005cafe1d41569c322ec7cfc8baf403e523ac
